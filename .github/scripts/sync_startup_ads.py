239dc2f1f5c11fdf17a2f16affde138f14bc1e8a
mrtanshiyue <54257052+mrtanshiyue@users.noreply.github.com>
2026-09-21 16:51:33 +0800

    fix: follow current NSRingo LocationServices release

--- .github/scripts/sync_startup_ads.py
+++ .github/scripts/sync_startup_ads.py
@@ -50,7 +50,7 @@ MIRROR_ALIASES = {
 
 SOURCE_OVERRIDES = {
     "https://raw.githubusercontent.com/VirgilClyne/iRingo/main/snippet/Location.snippet":
-        "https://github.com/NSRingo/GeoServices/releases/latest/download/Location.sgmodule",
+        "https://github.com/NSRingo/LocationServices/releases/latest/download/iRingo.LocationService.sgmodule",
 }
 
 DIRECTORY_ONLY_NAMES = {"更多应用去广告"}
